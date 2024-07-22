0b98c27ab1d7edb30f9414b75030c63906bbbd27
mkeloo <mkeloo@ufl.edu>
2024-07-21 21:04:53 -0400

    added local storage.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="max-w-7xl mx-auto w-full ">
       <div className="w-full flex justify-center items-center px-10 ">
-        <h1 className="text-4xl  md:text-5xl  p-4 m-4">Warehouse Items</h1>
+        <h1 className="text-3xl md:text-5xl p-4 m-4">Warehouse Items</h1>
       </div>
       <div className=" flex flex-col items-center justify-center px-0 md:px-10">
         <List />
